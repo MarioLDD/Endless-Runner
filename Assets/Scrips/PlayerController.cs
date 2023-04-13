994aff225dc47791d3f74a1ab190b390011ecd74
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2023-04-13 03:59:03 -0300

    varias cosas

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator animator;
     private float horizontalInput;
     public float lateralSpeed;
     public float jumpForce;
+    Vector3 targetPosition = (Vector3.right);
+    public float smoothTime = 0.5f;
+    public float speed = 10f;
+    Vector3 velocity;
 
+    private bool IsJumping;
+    private bool IsGrounded;
 
 
 
@@ -17,7 +25,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-
+       animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,10 +40,28 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetTrigger("IsJumping");
+        }
+        if (playerRb.velocity.y <0)
+        {
+            animator.SetTrigger("IsJumping");
+        }
+        
 
+
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, speed);
         }
 
-
-
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, -targetPosition, ref velocity, smoothTime, speed);
+        }
     }
 }
+ 
